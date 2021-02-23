698dec6224a13da1055a577a1ce3592f215b9da5
Михаил Тигин <tiginmike@gmail.com>
2021-02-23 13:08:51 +0300

    Last changes

--- SendTempToEa2/SendTempToEa2.ino
+++ SendTempToEa2/SendTempToEa2.ino
@@ -47,8 +47,8 @@ iarduino_AM2320 sensor;									//  iarduino_AM2320
 
 static byte bl999_data[BL999_DATA_ARRAY_SIZE] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-static volatile boolean bl999_sendallbits = false;
-static volatile boolean bl999_message_ready = false;
+//static volatile boolean bl999_sendallbits = false;
+//static volatile boolean bl999_message_ready = false;
 
 void setup() {
 	Serial.begin(115200);
@@ -64,6 +64,9 @@ void setup() {
 	bl999_data[7] = 15; //(int)((humidity & 240) >> 4);
 }
 void loop() {
+	unsigned long startHIpulselenth = millis();
+
+
 	ms = millis();
 		// Событие срабатывающее каждые 500 мс   
 		if ((ms - ms1) > 500 || ms < ms1) {
