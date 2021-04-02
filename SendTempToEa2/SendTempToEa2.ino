2afac9ff37aa325472f3d50378284e6c68d300f7
Михаил Тигин <tiginmike@gmail.com>
2021-04-02 21:27:41 +0300

    power safe mode on

--- SendTempToEa2/SendTempToEa2.ino
+++ SendTempToEa2/SendTempToEa2.ino
@@ -6,6 +6,7 @@
 
 #include <Wire.h>
 #include <iarduino_AM2320.h>
+#include <Narcoleptic.h>
 
 iarduino_AM2320 sensor;									//  iarduino_AM2320
 #define prd 4											// pin DATA  FS1000A 
@@ -24,7 +25,7 @@ iarduino_AM2320 sensor;									//  iarduino_AM2320
 #define BL999_BIT_0_LENGTH 1850
 
 //30 sec length
-#define BL999_30_SEC_LENGTH 27000000
+#define BL999_30_SEC_LENGTH 27000
 
 #define BL999_DATA_ARRAY_SIZE 9
 
@@ -42,8 +43,6 @@ unsigned long timingtocompare = 0;
 int pulseis = HIGH;
 static volatile unsigned long timelenth = 0;
 int bitnumber = 36;
-boolean startbit = false;
-boolean endbit = false;
 int j = 4;
 int sum = 0;
 int temperature = 0;
@@ -59,12 +58,33 @@ void setup() {
 	bl999_data[0] = PowerID >> 2;	
 	bl999_data[1] = (((PowerID << 2) | (CHANNEL << 1) | (CHANNEL >> 1)) & 15);
 	bl999_data[2] = 0;											//battery condition OK	
+
+	
+	// Nacroleptic treaks															
+	// Narcoleptic.disableMillis(); Do not disable millis - we need it for our delayCal() function.
+	Narcoleptic.disableTimer1();
+	Narcoleptic.disableTimer2();
+	Narcoleptic.disableSerial();
+	//  Narcoleptic.disableADC();  // !!! enabling this causes >100uA consumption !!!
+	//Narcoleptic.disableWire();
+	//Narcoleptic.disableSPI();
+
+	// Another tweaks to lower the power consumption
+	ADCSRA &= ~(1 << ADEN); //Disable ADC
+	ACSR = (1 << ACD); //Disable the analog comparator
+	//
+	// ATmega48/88/168/328
+	DIDR0 = B00111111; //Disable digital input buffers on all ADC0-ADC5 pins
+	DIDR1 = (1 << AIN1D) | (1 << AIN0D); //Disable digital input buffer on AIN1/0
+	//
+	// ATtiny25/45/85
+	//Disable digital input buffers on all ADC0-ADC3 + AIN1/0 pins
+	//DIDR0 = (1<<ADC0D)|(1<<ADC2D)|(1<<ADC3D)|(1<<ADC1D)|(1<<AIN1D)|(1<<AIN0D);
 }
 
 void loop() {
 	timing = micros();
 	if ((timing - timingtocompare) > timelenth ) {
-		//stamp();
 		timingtocompare = timing;
 		if (pulseis == HIGH) {
 			if (bitnumber > 35) {
@@ -82,7 +102,7 @@ void loop() {
 			}
 		}
 		else { 
-			timelenth = BL999_DIVIDER_PULSE_LENGTH;			
+			timelenth = 		
 			++bitnumber;			
 		}
 		// Инвертируем pulseis
@@ -103,12 +123,12 @@ void loop() {
 				sum += bl999_data[i];
 			}
 			sum &= 15;	// clear higher bits
-			bl999_data[BL999_DATA_ARRAY_SIZE - 1] = sum;	//calculat check s
-			timelenth = BL999_30_SEC_LENGTH;			
+			bl999_data[BL999_DATA_ARRAY_SIZE - 1] = sum;	//calculat check s			
+			Narcoleptic.delay(BL999_30_SEC_LENGTH);
+			timelenth = 0;			
 		}
 	}
 }	
-
 byte _bl999_GetbitfromDataArray(byte bitNumber) {
 	byte dataArrayIndex = bitNumber / BL999_BITS_PER_PACKET;
 	byte bitInNibble = bitNumber % 4;
